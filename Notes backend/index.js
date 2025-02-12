fe80fe59574617e7f894d3b55e9ad4e3c1a00dd5
GonzalezNacho <nacho.ldm63@gmail.com>
2025-02-12 13:52:50 -0300

    deploy de front notas en render

--- Notes backend/index.js
+++ Notes backend/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 let notes = [
     {
@@ -32,10 +33,6 @@ const requestLogger = (request, response, next) => {
 }
 
 app.use(requestLogger)
-
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
 
 app.get('/api/notes', (request, response) => {
     response.json(notes)
